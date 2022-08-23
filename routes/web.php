61dca77eacd980522ba2dd4257ab5a874fb9fe58
Yuds <codeyuda@gmail.com>
2022-08-23 09:04:11 +0700

    infopasar

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::group(['middleware' => 'auth'], function () {
 	 Route::get('map', function () {return view('pages.maps');})->name('map');
 	 Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
 	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
+	 Route::get('infopasar', function () {return view('pages.info_pasar_table');})->name('infopasar');
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
 
